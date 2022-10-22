0932801800d47c0baabd72cd23b2c60213b7f67b
Arijit Paria <tutoriex2020@gmail.com>
2022-10-22 23:45:01 +0530

    Update popup.js and popup.css

---
+++ popup.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", runFunction);
+console.log("DOM is loaded!")
+function runFunction() {
+  let button = document.getElementById("button1");
+  button.addEventListener("click", clickFunction);
+
+  function clickFunction() {
+    document.body.style.backgroundColor = "blue";
+  }
+}
